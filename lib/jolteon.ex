a04e8df13cdfef3612604f3c0ab2257cd86e51c1
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-06-26 02:47:25 -0400

    add LED status endpoint

--- lib/jolteon.ex
+++ lib/jolteon.ex
@@ -39,6 +39,34 @@ defmodule Jolteon do
        ))
   end
 
+   get "/yellow_status" do
+     request = HTTPotion.post "https://api.particle.io/v1/devices/#{System.get_env("PHOTON_ID")}/LEDstatus", [
+      body:
+        "access_token=" <> URI.encode_www_form(System.get_env("PHOTON_ACCESS_TOKEN")),
+      headers: [ "Content-Type": "application/x-www-form-urlencoded" ]
+    ]
+
+    response = Poison.decode!(request.body)
+
+    case response["return_value"] do
+      1 ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Poison.encode! (%{ led: "yellow", on: true}))
+
+      0 ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Poison.encode! (%{ led: "yellow", on: false}))
+
+      _ ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Poison.encode! (%{ response: response}))
+    end
+   end
+
+
   match _ do
     send_resp(conn, 404, "Does Not Exist")
   end
