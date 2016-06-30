4419475bc92172af5442ee0737a068d8cb3a28ae
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-06-30 00:05:53 -0400

    add photon function to toggle brightness of LED strip

--- photon/main.ino
+++ photon/main.ino
@@ -1,44 +1,43 @@
-// This #include statement was automatically added by the Particle IDE.
 #include "neopixel/neopixel.h"
 
-// ------------
-// Blink a RED and YELLOW LED
-// ------------
+#define PIN 6
 
+// ------------
+// JOLTEON
+// ------------
+#define RED_LED_PIN D0
+#define YELLOW_LED_PIN D1
+#define PIXEL_PIN 6
+#define PIXEL_COUNT 240
+#define PIXEL_TYPE WS2812B
 
-int redLED = D0;
-int yellowLED = D1;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 void setup() {
+    // Initialize PINS
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(YELLOW_LED_PIN, OUTPUT);
+    strip.begin();
 
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
+    // Initialize Photon endpoints
+    Particle.function("yellowLED",ledToggle);
+    Particle.function("LEDstatus",ledStatus);
+    Particle.function("ls",lsON);
+    Particle.function("ls_rgb",lsRGB);
 
-  Particle.function("yellowLED",ledToggle);
-  Particle.function("LEDstatus",ledStatus);
-  Particle.function("ls",lsON);
-  Particle.function("ls_rgb",lsRGB);
-
-  digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, LOW);
-
+    // Shut down all power to output pins
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    strip.show();
 }
 
-
 void loop() {
-
-  digitalWrite(redLED, HIGH);
-
-  delay(10000);
-
-  digitalWrite(redLED, LOW);
-
-  delay(10000);
-
+    digitalWrite(RED_LED_PIN, HIGH);
+    rainbow(20);
 }
 
 int ledStatus(String command) {
-    switch (digitalRead(yellowLED)) {
+    switch (digitalRead(YELLOW_LED_PIN)) {
     case HIGH:
         return 1;
         break;
@@ -54,11 +53,11 @@ int ledStatus(String command) {
 
 int ledToggle(String command) {
 
-    if (digitalRead(yellowLED) == HIGH) {
-        digitalWrite(yellowLED,LOW);
+    if (digitalRead(YELLOW_LED_PIN) == HIGH) {
+        digitalWrite(YELLOW_LED_PIN,LOW);
         return 0;
-    } else if (digitalRead(yellowLED) == LOW) {
-        digitalWrite(yellowLED,HIGH);
+    } else if (digitalRead(YELLOW_LED_PIN) == LOW) {
+        digitalWrite(YELLOW_LED_PIN,HIGH);
         return 1;
     } else {
         return -1;
@@ -66,9 +65,46 @@ int ledToggle(String command) {
 }
 
 int lsON(String command) {
-    return 1;
+    if (command == "true"){
+        strip.setBrightness(200);
+        strip.show();
+        return 1;
+    } else if (command == "false") {
+        strip.setBrightness(0);
+        strip.show();
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
 int lsRGB(String command) {
     return 1;
+}
+
+void rainbow(uint8_t wait) {
+    uint16_t i, j;
+
+    for(j=0; j<256; j++) {
+        for(i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel((i+j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
